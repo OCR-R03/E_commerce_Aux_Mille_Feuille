4289153f90e5c7b15186bda44cc8599213f0c08d
Carpenter <contact7841@gmail.com>
2022-03-16 22:34:25 +0100

    commit

--- Client/src/Pages/Connexion/Connexion.jsx
+++ Client/src/Pages/Connexion/Connexion.jsx
@@ -4,6 +4,9 @@ import {useNavigate, Link} from 'react-router-dom'
 import FacebookIcon from '../../Assets/Logo_Social/f_logo_RGB-Blue_58.png'
 import GoogleIcon from '../../Assets/Logo_Social/logo_google.svg'
 import { loginInitiate, googleSignInInitiate, fbSignInInitiate } from '../../Redux/actions'
+import { Formik, Form, Field, ErrorMessage } from "formik"
+import * as Yup from "yup" 
+import "bootstrap/dist/css/bootstrap.css";
 
 
 const Connexion = () => {
@@ -50,80 +53,150 @@ const Connexion = () => {
         setState({ ...state, [name]: value })
     }
 
+    const ConnexionSchema = Yup.object().shape({
+        email: Yup.string()
+        .email("Format de l'adresse email incorrect")
+        .required("Email obligatoire"),
+      
+        password: Yup.string()
+        .min(3, "Le mot de passe doit contenir 3 caratères au minimun")
+        .required("Mot de passe obligatoire"),
+    });
+
     return (
-        <div className='Container'>
-            <div className='logreg-forms'>
+        
+        <div className="Container_Connex">
+            <div className="row">
+                <div className="col-lg-12">
+                    <Formik 
+                        initialValues={{ email: "", password: "" }}
+                        validationSchema={ConnexionSchema}
+                        onSubmit={(values) => {
+                            handleSubmit
+                            console.log(values); 
+                            alert("Form is validated! Submitting the form...");
+                        }}
+                    >
+                        {({ touched, errors, isSubmitting, values }) =>
+                        !isSubmitting ? (
+                        
+                        <div>
+                            
+                            <div className="row mb-5">
+                                
+                                <div className="col-lg-12 text-center">
+                                    
+                                    <h1 className="mt-5">Se connecter</h1>
+                                
+                                </div>
+                            </div>
+                            
+                            <Form>
+                                
+                                <div className="form-group">
+                                    
+                                    <Field
+                                        type="email" 
+                                        id="inputEmail" 
+                                        placeholder="Adresse email" 
+                                        name="email"
+                                        onChange={handleChange} 
+                                        value={email}
+                                        required
+                                        autocomplete="off"
+                                        className={`col-lg-12 text-center" 
+                                        ${touched.email && errors.email ? "is-invalid" : ""}`}
+                                    />
+                                    
+                                    <ErrorMessage
+                                        component="div"
+                                        name="email"
+                                        className="invalid-feedback"
+                                    />
+                                </div>
+                        
+                                <div className="form-group">
+                            
+                                    <Field
+                                        type="password" 
+                                        id="inputPassword"  
+                                        placeholder="Mot de passe" 
+                                        name="password"
+                                        onChange={handleChange} 
+                                        value={password}
+                                        required
+                                        className={`mt-2 form-control
+                                        ${
+                                        touched.password && errors.password
+                                            ? "is-invalid"
+                                            : ""
+                                        }`}
+                                    />
+                            
+                                    <ErrorMessage
+                                        component="div"
+                                        name="password"
+                                        className="invalid-feedback"
+                                    />
+                        
+                                </div>
 
-                <form className='form-signin' onSubmit={handleSubmit}>
-                    <h1 className='h3 mb-3 font-weight-normal' style={{textAlign: "center", color: 'white'}}>
-                        Se connecter
-                    </h1>
+                                <br/>
 
-                    <input 
-                    type="email" 
-                    id="inputEmail" 
-                    className="form-control" 
-                    placeholder="Adresse email" 
-                    name="email"
-                    onChange={handleChange} 
-                    value={email}
-                    required
-                    />
+                                <Link to="/SuccessConnexion">
+                                <button className='btn-connecter' type="submit">
+                                    <i className="fas fa-sign-in-alt"></i> Se connecter
+                                </button>
+                                </Link>
 
-                    <br/>
+                                <hr />
+                                
+                                <p style={{textAlign: "center", color: 'white'}}>OU</p>
 
-                    <input 
-                    type="password" 
-                    id="inputPassword" 
-                    className="form-control" 
-                    placeholder="Mot de passe" 
-                    name="password"
-                    onChange={handleChange} 
-                    value={password}
-                    required
-                    />
+                                <div className='social-login'>
+                                    <button className='btn google-btn social-btn' type="button" onClick={handleGoogleSignIn}>
+                                        <span>
+                                            <img className='Logo_Google' src={GoogleIcon} alt="Logo Google"></img> Se connecter via Google +
+                                        </span>
+                                    </button>
+                                </div>
+                                
+                                <br/>
 
-                    <br/>
+                                <div className='social-login2'>
+                                    <button className='btn facebook-btn social-btn' type="button" onClick={handleFBSignIn}>
+                                        <span>
+                                            <img className='Logo_Facebook' src={FacebookIcon} alt="Logo Facebook"></img> Se connecter via Facebook
+                                        </span>
+                                    </button>
+                                </div>
+
+                                <p style={{display: 'flex', justifyContent: 'center', color: "white", paddingTop: 30}}>Vous n'avez pas de compte ?</p>
+                                
+                                <div className='box_create_account'>
+                                    <Link to='/Senregistrer'>
+                                        <button
+                                            className='create_account'
+                                            type='button'
+                                            id="btn-signup"
+                                    >
+                                            <i className='fas fa-user-plus'></i> Créer un nouveau compte
+                                        </button>
+                                    </Link>  
+                                </div>
+
+                            </Form>
+                        </div>
+                    ) : (
                     
-                    <Link to="/SuccessConnexion">
-                    <button className='btn-connecter' type="submit">
-                        <i className="fas fa-sign-in-alt"></i> Se connecter
-                    </button>
-                    </Link>
-
-                    <hr />
-                    <p style={{textAlign: "center", color: 'white'}}>OU</p>
-
-                    <div className='social-login'>
-                        <button className='btn google-btn social-btn' type="button" onClick={handleGoogleSignIn}>
-                            <span>
-                                <img className='Logo_Google' src={GoogleIcon} alt="Logo Google"></img> Se connecter via Google +
-                            </span>
-                        </button>
-                    </div>
-
-                    <div className='social-login2'>
-                        <button className='btn facebook-btn social-btn' type="button" onClick={handleFBSignIn}>
-                                <span>
-                                    <img className='Logo_Facebook' src={FacebookIcon} alt="Logo Facebook"></img> Se connecter via Facebook
-                                </span>
-                        </button>
-                    </div>
-
-                    <p style={{display: 'flex', justifyContent: 'center', color: "white", paddingTop: 30}}>Vous n'avez pas de compte ?</p>
-                    <Link to='/Senregistrer'>
-                        <button
-                            className='btn_create_account'
-                            type='button'
-                            id="btn-signup"
-                        >
-                            <i className='fas fa-user-plus'></i> Créer un nouveau compte
-                        </button>
-                    </Link>    
-                </form> 
+                    <div></div>
+                    )
+                }
+                </Formik>
+            </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default Connexion
---
+++ Client/src/Pages/Connexion/Connexion2.jsx
@@ -0,0 +1,75 @@
+<div className='Container_Connex'>
+    
+            <div className='logreg-forms'>
+
+                <form className='form-signin' onSubmit={handleSubmit}>
+                    <h1 className='h3 mb-3 font-weight-normal' style={{textAlign: "center", color: 'white'}}>
+                        Se connecter
+                    </h1>
+
+                    <input 
+                    type="email" 
+                    id="inputEmail" 
+                    className="form-control" 
+                    placeholder="Adresse email" 
+                    name="email"
+                    onChange={handleChange} 
+                    value={email}
+                    required
+                    />
+
+                    <br/>
+
+                    <input 
+                    type="password" 
+                    id="inputPassword" 
+                    className="form-control" 
+                    placeholder="Mot de passe" 
+                    name="password"
+                    onChange={handleChange} 
+                    value={password}
+                    required
+                    />
+
+                    <br/>
+                    
+                    <Link to="/SuccessConnexion">
+                    <button className='btn-connecter' type="submit">
+                        <i className="fas fa-sign-in-alt"></i> Se connecter
+                    </button>
+                    </Link>
+
+                    <hr />
+                    <p style={{textAlign: "center", color: 'white'}}>OU</p>
+
+                    <div className='social-login'>
+                        <button className='btn google-btn social-btn' type="button" onClick={handleGoogleSignIn}>
+                            <span>
+                                <img className='Logo_Google' src={GoogleIcon} alt="Logo Google"></img> Se connecter via Google +
+                            </span>
+                        </button>
+                    </div>
+
+                    <div className='social-login2'>
+                        <button className='btn facebook-btn social-btn' type="button" onClick={handleFBSignIn}>
+                                <span>
+                                    <img className='Logo_Facebook' src={FacebookIcon} alt="Logo Facebook"></img> Se connecter via Facebook
+                                </span>
+                        </button>
+                    </div>
+
+                    <p style={{display: 'flex', justifyContent: 'center', color: "white", paddingTop: 30}}>Vous n'avez pas de compte ?</p>
+                    <Link to='/Senregistrer'>
+                        <button
+                            className='btn_create_account'
+                            type='button'
+                            id="btn-signup"
+                        >
+                            <i className='fas fa-user-plus'></i> Créer un nouveau compte
+                        </button>
+                    </Link>    
+                </form> 
+            </div>
+        </div>
+    )
+}
